9dd8bd043f6578668f9d0aa632e05508405407ac
Yonathan Castillo <leothan522@gmail.com>
2021-01-10 00:28:55 -0400

    Actualizacion Archivo Conexion

--- conexion.php
+++ conexion.php
@@ -1,12 +1,14 @@
 <?php
  header("Access-Control-Allow-Origin: *");
 
+require 'env.php';
+
 class Conexion{
 	public function get_conexion(){
-		$user = "root";
-		$pass = "";
-		$host = "localhost";
-		$db = "laravel-app";
+		$user = CONEXION_USER;
+		$pass = CONEXION_PASSWORD;
+		$host = CONEXION_HOST;
+		$db = CONEXION_DB;
 		$conexion = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 		return($conexion);
 	}
